64940d8515f6e6c4c74fb97fc274b01c0f4aa131
peter0749 <jengku@gmail.com>
2020-02-21 12:29:57 +0800

    fix parallelism

--- code/src/fps/fps.cpp
+++ code/src/fps/fps.cpp
@@ -10,36 +10,54 @@ std::vector<int> farthest_point_sampling(const Eigen::MatrixXd &pts, int M) {
     std::mt19937 mt(rd());
     int N = pts.rows();
     // int D = pts.cols();
-    ska::flat_hash_set<int> in_set; // indices to be return
+    ska::flat_hash_set<int> in_set; // lookup table
+    std::vector<int> in_set_vector; // indices to be return
+    std::vector<float> dist_vector(N, 0); 
     std::uniform_int_distribution<int> uniform_N(0, N-1);
-    int old_idx = uniform_N(mt);
     M = std::min(M, N);
-    in_set.insert(old_idx);
+    in_set.insert(uniform_N(mt));
     for (int j=1; j<M; ++j) {
-        Compare best(0, -1.0);
         #ifdef USE_OMP
-        #pragma omp parallel for reduction(maximum: best) shared(pts, old_idx, in_set, N)
+        #pragma omp parallel for shared(pts, in_set, in_set_vector, dist_vector)
         #endif
         for (int k=0; k<N; ++k) {
-            if (in_set.count(k)>0) continue;
+            if (in_set.count(k)>0) {
+                dist_vector[k] = 0.0;
+                continue;
+            }
             float min_dist = FLT_MAX;
-            for (auto v_in : in_set) {
+            for (auto v_in : in_set_vector) {
                 float dist = (pts.row(v_in)-pts.row(k)).norm();
-                if (dist<min_dist) {
+                if (dist<min_dist) { // Assign point to nearest in-set point
                     min_dist = dist;
                 }
             }
-            if (min_dist>best.second) {
-                best.first = k;
-                best.second = min_dist;
+            dist_vector[k] = min_dist;
+        }
+        int best_index = 0;
+        float best_dist = dist_vector[0];
+        #pragma omp parallel
+        {
+            int index_local = best_index;
+            float best_local = best_dist;  
+            #pragma omp for nowait
+            for (int i=1; i<dist_vector.size(); ++i) {
+                if (dist_vector[i]>best_local) {
+                    best_local = dist_vector[i];
+                    index_local = i;
+                }
+            }
+            #pragma omp critical 
+            {
+                if (best_local>best_dist) {
+                    best_dist = best_local;
+                    best_index = index_local;
+                }
             }
         }
-        in_set.insert(best.first);
-        old_idx = best.first;
+        in_set.insert(best_index);
+        in_set_vector.push_back(best_index);
     }
-    std::vector<int> in_set_vector; 
-    in_set_vector.reserve(in_set.size());
-    for (auto v : in_set) in_set_vector.push_back(v);
     return in_set_vector;
 }
 
@@ -55,35 +73,53 @@ std::vector<int> farthest_point_sampling(const Eigen::MatrixXd &pts, const std::
     std::mt19937 mt(rd());
     int N = mask.size();
     // int D = pts.cols();
-    ska::flat_hash_set<int> in_set; // indices to be return
+    ska::flat_hash_set<int> in_set; // lookup table
+    std::vector<int> in_set_vector; // indices to be return
+    std::vector<float> dist_vector(N, 0); 
     std::uniform_int_distribution<int> uniform_N(0, N-1);
-    int old_idx = uniform_N(mt);
     M = std::min(M, N);
-    in_set.insert(old_idx);
+    in_set.insert(uniform_N(mt));
     for (int j=1; j<M; ++j) {
-        Compare best(0, -1.0);
         #ifdef USE_OMP
-        #pragma omp parallel for reduction(maximum: best) shared(pts, old_idx, in_set, N, mask)
+        #pragma omp parallel for shared(pts, in_set, in_set_vector, dist_vector)
         #endif
-        for (int m=0; m<N; ++m) {
-            if (in_set.count(m)>0) continue;
+        for (int k=0; k<N; ++k) {
+            if (in_set.count(k)>0) {
+                dist_vector[k] = 0.0;
+                continue;
+            }
             float min_dist = FLT_MAX;
-            for (auto v_in : in_set) {
-                float dist = (pts.row(mask[v_in])-pts.row(mask[m])).norm();
-                if (dist<min_dist) {
+            for (auto v_in : in_set_vector) {
+                float dist = (pts.row(mask[v_in])-pts.row(mask[k])).norm();
+                if (dist<min_dist) { // Assign point to nearest in-set point
                     min_dist = dist;
                 }
             }
-            if (min_dist>best.second) {
-                best.first = m;
-                best.second = min_dist;
+            dist_vector[k] = min_dist;
+        }
+        int best_index = 0;
+        float best_dist = dist_vector[0];
+        #pragma omp parallel
+        {
+            int index_local = best_index;
+            float best_local = best_dist;  
+            #pragma omp for nowait
+            for (int i=1; i<dist_vector.size(); ++i) {
+                if (dist_vector[i]>best_local) { // find farthest point
+                    best_local = dist_vector[i];
+                    index_local = i;
+                }
+            }
+            #pragma omp critical 
+            {
+                if (best_local>best_dist) {
+                    best_dist = best_local;
+                    best_index = index_local;
+                }
             }
         }
-        in_set.insert(best.first);
-        old_idx = best.first;
+        in_set.insert(best_index);
+        in_set_vector.push_back(best_index);
     }
-    std::vector<int> in_set_vector; 
-    in_set_vector.reserve(in_set.size());
-    for (auto v : in_set) in_set_vector.push_back(v);
     return in_set_vector;
 }
